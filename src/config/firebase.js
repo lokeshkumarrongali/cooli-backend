aaf0a1087711e342088e86e4fcf94b375d5897fb
lokesh-love <lokeshkumarrongali@gmail.com>
2026-03-15 18:37:04 +0530

    fix: robust private key parsing for PEM format

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,7 +7,9 @@ console.log("Private Key Exists:", !!process.env.FIREBASE_PRIVATE_KEY);
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY 
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/^"(.*)"$/, '$1') 
+    : undefined,
 };
 
 if (!serviceAccount.projectId || !serviceAccount.clientEmail || !serviceAccount.privateKey) {
