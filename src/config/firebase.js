c77467f7f28497c23fbff52acf553b7fc8abb292
lokesh-love <lokeshkumarrongali@gmail.com>
2026-03-15 09:32:28 +0530

    Fix CORS regex and axios global api suffix

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,7 +4,7 @@ const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKey: process.env.FIREBASE_PRIVATE_KEY
-    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/^"|"$/g, '').replace(/\\n/g, "\n")
     : undefined,
 };
 
